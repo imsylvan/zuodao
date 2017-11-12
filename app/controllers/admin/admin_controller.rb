a2b217b794b3343b91bfcb5c8f471086076c595b
sylvan <sylzzz@foxmail.com>
2017-11-12 22:21:43 +0800

    222

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,4 +1,6 @@
 class Admin::AdminController < ApplicationController
-
+  before_action :authenticate_user!
+  before_action :save_back_url
+  before_action :admin_required, except: [:index, :new, :edit, :show]
   layout "admin"
 end
